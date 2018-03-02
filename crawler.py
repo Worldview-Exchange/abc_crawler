9bb97079e2f08eca68b47f657037bf98908825d8
Liam Cripwell <ljcripwell@gmail.com>
2018-03-02 10:43:07 +1000

    added cmdln arg support

--- crawler.py
+++ crawler.py
@@ -36,6 +36,35 @@ def outputToPdf(articles, file_name):
     df = pd.DataFrame(articles, columns=["content"])
     df.to_csv(file_name)
 
-topic = crawlTopic("winter-olympics", 5)
-articles = crawlArticles(topic)
-outputToPdf(articles, "test_crawl.csv")
+
+import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:n:o:",["topic=","num=","ofile="])
+   except getopt.GetoptError:
+      print('crawler.py -i <topic> -n <number_of_articles> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('crawler.py -i <topic> -n <number_of_articles> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--topic"):
+         topic = arg
+      elif opt in ("-n", "--num"):
+          # TODO: handle type incompatibility
+         num = int(arg)
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print('Crawling %d articles from topic \"%s\"...' % (num, topic))
+   topic = crawlTopic(topic, num)
+   articles = crawlArticles(topic)
+   print('Writing files to %s...' % outputfile)
+   outputToPdf(articles, outputfile)
+   print("Done...")
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
